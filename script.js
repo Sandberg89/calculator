8273a8382c232ec3b5bbc3b6a4dc36758486d145
Linus <linus.sandberg@live.se>
2022-03-26 12:23:32 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+
+
+// functions
+function operate(operator, a, b){
+    if (operator ==='+'){
+        return add(a, b);
+    }
+    else if (operator ==='-'){
+        return subtract(a, b);
+    }
+    else if (operator ==='*'){
+        return multiply(a, b);
+    }
+    else if (operator ==='/'){
+        return divide(a, b);
+    }
+    else if (operator ==='%'){
+        return modulus(a, b);
+    }
+}
+
+
+function add(a, b){
+    return a + b; 
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function modulus(a, b) {
+    return a % b;
+}
