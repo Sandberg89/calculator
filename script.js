32969d5d8a74823d5abdba4788eee0c57536bc32
Linus <linus.sandberg@live.se>
2022-04-05 20:21:21 +0200

    Added some more operations

--- script.js
+++ script.js
@@ -1,41 +1,87 @@
-
+let displayElement = document.getElementById("display");
+let display = "";
+const numberBtns = document.getElementsByClassName("numb");
+const btnArray = Array.from(numberBtns);
+const opsBtns = document.getElementsByClassName("ops");
+const opsArray = Array.from(opsBtns);
+let clearBtn = document.getElementById("cls");
+let tempNumber = "";
+let operation = "";
 
 // functions
-function operate(operator, a, b){
-    if (operator ==='+'){
-        return add(a, b);
-    }
-    else if (operator ==='-'){
-        return subtract(a, b);
-    }
-    else if (operator ==='*'){
-        return multiply(a, b);
-    }
-    else if (operator ==='/'){
-        return divide(a, b);
-    }
-    else if (operator ==='%'){
-        return modulus(a, b);
-    }
+function operate(operator, a, b) {
+  if (operator === "+") {
+    return add(a, b);
+  } else if (operator === "-") {
+    return subtract(a, b);
+  } else if (operator === "*") {
+    return multiply(a, b);
+  } else if (operator === "/") {
+    return divide(a, b);
+  } else if (operator === "%") {
+    return modulus(a, b);
+  }
 }
 
-
-function add(a, b){
-    return a + b; 
+function add(a, b) {
+  return a + b;
 }
 
 function subtract(a, b) {
-    return a - b;
+  return a - b;
 }
 
-function multiply(a, b){
-    return a * b;
+function multiply(a, b) {
+  return a * b;
 }
 
 function divide(a, b) {
-    return a / b;
+  return a / b;
 }
 
 function modulus(a, b) {
-    return a % b;
+  return a % b;
 }
+
+btnArray.forEach((element) => {
+  element.addEventListener("click", (e) => {
+    let btnValue = e.target.innerText;
+    if (btnValue == "," && display == "") {
+      display += "0" + btnValue;
+      displayElement.innerText = display;
+    } else if (display == "0" && btnValue != ',') {
+      display = btnValue;
+      displayElement.innerText = display;
+    } else {
+      display += btnValue;
+      displayElement.innerText = display;
+    }
+  });
+});
+
+opsArray.forEach((element) => {
+  element.addEventListener("click", (e) => {
+    if (tempNumber == "") {
+      tempNumber = display;
+      display = "";
+      operation = e.target.innerText;
+      console.log(`display: ${display}, tempNumber: ${tempNumber}`);
+    } else {
+      tempNumber = operate(
+        operation,
+        parseFloat(tempNumber),
+        parseFloat(display)
+      );
+      displayElement.innerText = tempNumber;
+      display = "";
+      operation = e.target.innerText;
+      console.log(`display: ${display}, tempNumber: ${tempNumber}`);
+    }
+  });
+});
+
+clearBtn.addEventListener("click", () => {
+  tempNumber = "";
+  display = "";
+  displayElement.innerText = display;
+});
